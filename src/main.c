524b0b9c6771a08b2c7908e5b1e986abe2cbb3cf
Borromeu <adelin_andrei.fulop@stud.acs.upb.ro>
2024-11-19 13:34:45 +0200

    check all arguments received

--- src/main.c
+++ src/main.c
@@ -1,4 +1,12 @@
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+bool file_exists (char *filename) {
+    struct stat buffer;   
+    return (stat (filename, &buffer) == 0);
+}
 
 int main(int argc, char **argv)
 {
@@ -7,5 +15,22 @@ int main(int argc, char **argv)
         exit(1);
     }
     
+    int numberOfMappers = atoi(argv[1]);
+    int numberOfReducers = atoi(argv[2]);
+
+    if (numberOfMappers < 1) {
+        printf("Numar incorect de mappers sau reducers\n");
+        exit(1);
+    }
+
+    if (numberOfReducers < 1) {
+        printf("Numar incorect de mappers sau reducers\n");
+        exit(1);
+    }
+
+    if (!file_exists(argv[3])) {
+        printf("Fisierul %s nu exista\n", argv[3]);
+        exit(1);
+    }
     return 0;
 }
